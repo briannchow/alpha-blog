853d7a589a49851d08b25b71e687847ac18a2063
Chow <briannnnchow@gmail.com>
2016-05-05 06:33:04 +0000

    added an index page along with links.etc

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,25 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
     
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
   
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was edited succesfully"
+      redirect_to article_path(@article)
+    else
+      render "edit"
+    end
+  end
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
@@ -29,6 +45,6 @@ class ArticlesController < ApplicationController
     
     end
   
-    
+  
   
 end
